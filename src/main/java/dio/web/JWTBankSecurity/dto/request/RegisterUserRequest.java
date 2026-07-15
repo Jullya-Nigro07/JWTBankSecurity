2bdc28a27d6e30a78ec84dab13c08c60fa49ee94
Jullya-Nigro07 <nigrojullya07@gmail.com>
2026-07-15 15:54:24 -0300

    refactor: remover mensagens

--- src/main/java/dio/web/JWTBankSecurity/dto/request/RegisterUserRequest.java
+++ src/main/java/dio/web/JWTBankSecurity/dto/request/RegisterUserRequest.java
@@ -1,8 +1,8 @@
 package dio.web.JWTBankSecurity.dto.request;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record RegisterUserRequest(@NotEmpty(message = "Name is required")String name, @Email @NotEmpty(message = "E-mail is required")String email,@Size(min = 7, max = 15) @NotEmpty(message = "Password is required")String password) {
+public record RegisterUserRequest(@NotBlank String name, @NotBlank @Email String email, @NotBlank @Size(min = 7, max = 15) String password) {
 }
