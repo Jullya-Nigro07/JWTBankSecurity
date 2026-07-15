b016b821dee2912da02d3da709fa3390209be7ad
Jullya-Nigro07 <nigrojullya07@gmail.com>
2026-07-15 15:52:34 -0300

    refactor: remove classe obsoleta

--- src/main/java/dio/web/JWTBankSecurity/exception/GlobalExceptionHandler.java
+++ src/main/java/dio/web/JWTBankSecurity/exception/GlobalExceptionHandler.java
@@ -1,22 +1,19 @@
 package dio.web.JWTBankSecurity.exception;
 
-import dio.web.JWTBankSecurity.dto.erro.UnauthorizedException;
 import dio.web.JWTBankSecurity.dto.erro.ErroRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.util.HashMap;
 import java.util.Map;
-
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErroRequest> handlerCampoInvalidoRegister(MethodArgumentNotValidException ex) {
+    public ResponseEntity<ErroRequest> handlerInvalidFieldRegister(MethodArgumentNotValidException ex) {
         Map<String, String> error = new HashMap<>();
 
         ex.getBindingResult().getFieldErrors().forEach(erro -> error.put(erro.getField(), erro.getDefaultMessage()));
@@ -28,11 +25,4 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.badRequest().body(response);
     }
-
-    @ExceptionHandler(dio.web.JWTBankSecurity.exception.UnauthorizedException.class)
-    public ResponseEntity<UnauthorizedException> handlerUnauthorizedException(dio.web.JWTBankSecurity.exception.UnauthorizedException ex){
-        UnauthorizedException unauthorizedException = new UnauthorizedException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
-        return ResponseEntity.badRequest().body(unauthorizedException);
-    }
 }
-
