402e3a15aefca1635b18a5e8a45205f000399052
Jullya-Nigro07 <nigrojullya07@gmail.com>
2026-07-15 15:54:48 -0300

    refactor: remover imports nao utilizados

--- src/main/java/dio/web/JWTBankSecurity/JwtBankSecurityApplication.java
+++ src/main/java/dio/web/JWTBankSecurity/JwtBankSecurityApplication.java
@@ -1,10 +1,7 @@
 package dio.web.JWTBankSecurity;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Scanner;
 
 @SpringBootApplication
 public class JwtBankSecurityApplication {
