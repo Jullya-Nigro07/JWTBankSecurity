17eb0a61a83821bd86e4b4bbd02d85ec1f748eda
Jullya-Nigro07 <nigrojullya07@gmail.com>
2026-07-15 15:43:35 -0300

    refactor: ajustar tipos de id no padrao sql e nao postgres e boa pratica do not null nas colunas

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,24 +1,24 @@
 CREATE TABLE users(
-   id serial PRIMARY KEY,
-
-   name varchar(255),
-   email varchar (255) UNIQUE,
-   password varchar(255)
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    email varchar (255) UNIQUE NOT NULL,
+    password varchar(255) NOT NULL
 );
 
 CREATE TABLE accounts(
-    id serial PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     balance NUMERIC(15,2) NOT NULL DEFAULT 0,
     user_id BIGINT UNIQUE NOT NULL,
 
     CONSTRAINT fk_user_account FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
 CREATE TABLE transactions(
-    id serial PRIMARY KEY,
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     account_id BIGINT NOT NULL,
     type varchar(20) NOT NULL,
     amount NUMERIC(15,2) NOT NULL,
-    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    date_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT fk_account_transaction FOREIGN KEY (account_id) REFERENCES accounts(id)
 );
