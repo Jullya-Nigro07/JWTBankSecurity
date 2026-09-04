2be88c81556265ad6479660b8cd27778dc7e77df
Jullya-Nigro07 <nigrojullya07@gmail.com>
2026-09-04 17:15:21 -0300

    test: adiciona testes com mock da autenticação por token

--- src/test/java/dio/web/JWTBankSecurity/service/UserServiceTest.java
+++ src/test/java/dio/web/JWTBankSecurity/service/UserServiceTest.java
@@ -2,31 +2,66 @@ package dio.web.JWTBankSecurity.service;
 
 import dio.web.JWTBankSecurity.dto.request.LoginRequest;
 import dio.web.JWTBankSecurity.dto.request.RegisterUserRequest;
+import dio.web.JWTBankSecurity.dto.request.UpdateUserRequest;
 import dio.web.JWTBankSecurity.dto.response.LoginResponse;
+import dio.web.JWTBankSecurity.dto.response.UserResponse;
+import dio.web.JWTBankSecurity.entity.User;
 import dio.web.JWTBankSecurity.exception.ConflitInfoException;
+import dio.web.JWTBankSecurity.repository.UserRepository;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class UserServiceTest{
+class UserServiceTest {
+
+    @MockitoBean
+    private AuthorizationService authorizationService;
 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        // Inicializa o validador correto do Jakarta/Hibernate
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        this.validator = factory.getValidator();
+    }
+
     @Test
-    void shouldValidateDto(){
-        RegisterUserRequest user = new RegisterUserRequest("Teste", "testdto@teste.com", "1234567");
-        assertAll(
-                () -> assertFalse(user.name().isBlank()),
-                () -> assertTrue(user.email().contains("@")),
-                () -> assertTrue(user.password().length() >= 7 && user.password().length() <= 15)
-        );
+    void shouldInvalidDto(){
+        RegisterUserRequest userDto = new RegisterUserRequest(" ", "testdtoteste.com", "123456");
+        Set<ConstraintViolation<RegisterUserRequest>> violacoes = validator.validate(userDto);
+        assertEquals(3, violacoes.size(), "Existem violações de validação.");
+    }
+
+    @Test
+    void shouldValidDto(){
+        RegisterUserRequest userDto = new RegisterUserRequest("Teste", "testdto@teste.com", "123456789");
+        Set<ConstraintViolation<RegisterUserRequest>> violacoes = validator.validate(userDto);
+        assertEquals(0, violacoes.size(), "Não existem violações de validação.");
     }
 
     @Test
@@ -38,6 +73,18 @@ class UserServiceTest{
         assertThrows(ConflitInfoException.class, () -> {
             userService.register(user2);
         });
+    }
+
+    @Test
+    void shouldSaveUser() {
+        RegisterUserRequest userRequest = new RegisterUserRequest("Teste", "testemail@teste.com", "1234567");
+        ResponseEntity<UserResponse> response = userService.register(userRequest);
+        User user = userRepository.findUserByEmail(response.getBody().email()).orElseThrow();
+
+        assertAll(
+                () -> assertNotNull(user),
+                () -> assertEquals(HttpStatus.OK, response.getStatusCode())
+        );
     }
 
     @Test
@@ -56,15 +103,38 @@ class UserServiceTest{
 
     @Test
     void shouldUpdateUser(){
-        RegisterUserRequest user = new RegisterUserRequest("Teste01", "teste1@teste.com", "1234567");
-        userService.register(user);
+        RegisterUserRequest userRequest = new RegisterUserRequest("Teste01", "teste01@teste.com", "1234567");
+        userService.register(userRequest);
 
-        //UpdateUserRequest userUpdate = new UpdateUserRequest("Teste02", null, null);
-        //<UserResponse> response = userService.updateUser(userUpdate);
+        User user = userRepository.findUserByEmail(userRequest.email()).orElseThrow();
+        when(authorizationService.getAuthenticatedUser()).thenReturn(user);
 
-        //assertAll(
-        //        () -> assertEquals(HttpStatus.OK, response.getStatusCode()),
-        //        () -> assertTrue(userUpdate.name().equals("Teste02"))
-        //);
+        UpdateUserRequest userRequest2 = new UpdateUserRequest("Teste02", "teste02@teste.com", "7654321");
+        ResponseEntity<UserResponse> response = userService.updateUser(userRequest2);
+
+        User userUpdate = userRepository.findUserByEmail(userRequest.email()).orElseThrow();
+
+        assertAll(
+                () -> assertEquals(HttpStatus.OK, response.getStatusCode()),
+                () -> assertEquals("Teste02", user.getName()),
+                () -> assertEquals("teste02@teste.com", user.getEmail()),
+                () -> assertTrue(passwordEncoder.matches("7654321", user.getPassword()))
+        );
+    }
+
+    @Test
+    void shouldDeleteUser(){
+        RegisterUserRequest userRequest = new RegisterUserRequest("Teste", "teste@teste.com", "1234567");
+        userService.register(userRequest);
+
+        User userRepo = userRepository.findUserByEmail(userRequest.email()).orElseThrow();
+        when(authorizationService.getAuthenticatedUser()).thenReturn(userRepo);
+
+        ResponseEntity<UserResponse> user = userService.deleteUser();
+
+        assertAll(
+                () -> assertTrue(userRepository.findUserByEmail(user.getBody().email()).isEmpty()),
+                () -> assertEquals(HttpStatus.OK, user.getStatusCode())
+        );
     }
 }
